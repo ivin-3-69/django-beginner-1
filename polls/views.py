6eaf5466a36fc36ac5bd0cb039bec4f3a80cbe68
ivin-3-69 <ivin.kuriakose.2000@gmail.com>
2020-08-25 21:04:15 +0530

    z

--- polls/views.py
+++ polls/views.py
@@ -14,8 +14,11 @@ def index(request):
     return render(request,'polls/index.html',context)
 
 def details(request ,question_id):
-    question = Question.objects.get(pk=question_id)
-    return render(request,'polls/details.html',{'question':question})
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request , question_id):
     response = "You're looking at the result of question %s"
