f663d1caaffb4ae9648f0cf42feb94a620b0190f
ivin-3-69 <ivin.kuriakose.2000@gmail.com>
2020-08-25 20:56:56 +0530

    added a html to index and details -a rough one

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
+from django.template import loader
+
+from .models import Question
 # Create your views here.
    
 def index(request):
-    return HttpResponse("Hello world, its django here.")
+    latest_question_list = Question.objects.all()
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
+
+def details(request ,question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request,'polls/details.html',{'question':question})
+
+def results(request , question_id):
+    response = "You're looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request ,question_id):
+    return("You're voting on question %s" % question_id)
