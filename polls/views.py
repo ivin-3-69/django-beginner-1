03e95c3016810c5b7fa61f57f3ac3dc29ad9a205
ivin-3-69 <ivin.kuriakose.2000@gmail.com>
2020-08-27 19:36:25 +0530

    voting system ON

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 
 from django.template import loader
@@ -13,16 +13,13 @@ def index(request):
     }
     return render(request,'polls/index.html',context)
 
-def details(request ,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+def detail(request ,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
 
 def results(request , question_id):
     response = "You're looking at the result of question %s"
     return HttpResponse(response % question_id)
 
 def vote(request ,question_id):
-    return("You're voting on question %s" % question_id)
+    return HttpResponse("You're voting on question %s" % question_id)
